100db8835c7f6a76f1f79f47fc2a95e14660d0a2
Ксения Уграк <x.ugrak@yandex.ru>
2023-10-07 21:14:29 +0300

    спринт 8 тема 1 урок 5

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -3,7 +3,10 @@ from django.conf import settings
 # Импортируем функцию, позволяющую серверу разработки отдавать файлы.
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -11,5 +14,14 @@ urlpatterns = [
     path('birthday/', include('birthday.urls')),
     # Подключаем urls.py приложения для работы с пользователями.
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
     # В конце добавляем к списку вызов функции static.
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
